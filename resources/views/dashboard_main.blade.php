0b05029db220c2973e9846cb2ffc7b5762ae7107
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-11-19 15:04:15 -0300

    fix - Ajuste no link de import do dashboard_main.js

--- resources/views/dashboard_main.blade.php
+++ resources/views/dashboard_main.blade.php
@@ -315,17 +315,10 @@
     </div>
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 main">
-
-
-      @yield('content')
-    
-
-      <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
-      
+      @yield('content')    
     </main>
   </div>
 </div>
 <script src="/assets/dist/js/bootstrap.bundle.min.js"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous"></script><script src="dashboard.js"></script></body>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous"></script><script src="/js/dashboard_main.js"></script></body>
 </html>
