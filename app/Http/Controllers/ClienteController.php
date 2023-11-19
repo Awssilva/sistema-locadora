033e6b70129ee07786ddd2e2a6533a12459d9e47
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-11-19 16:29:12 -0300

    fix - Ajustes e correções no ClienteController

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -8,46 +8,50 @@ use App\Models\Cliente;
 class ClienteController extends Controller
 {
     public function create(){
-        return view('cliente.cadastrar');
+        return view('cliente.consultar');
+    }
+
+    public function create_cadastro(){
+        return view('cliente.cadastro');
     }
     
     public function cadastrarCliente(Request $request)
     {
         $cliente = new Cliente();
 
-        $cliente->id_cliente = $request->id_cliente;
+        //$cliente->id_cliente = $request->id_cliente;
         $cliente->nome = $request->nome;
         $cliente->cpf = $request->cpf;
         $cliente->endereco = $request->endereco;
         $cliente->telefone = $request->telefone;
-        $cliente->data_cadastro = date('d/m/Y H:i');
+        $cliente->data_cadastro = date('Y-m-d H:i:s');
 
         $cliente->save();
 
-        return redirect('clientes/cadastrar')->with('cadastroRealizado', 'Cadastro realizado com sucesso!');
+        return redirect(route('cliente.view-menu'));//->with('cadastroRealizado', 'Cadastro realizado com sucesso!');
     }
 
     public function consultarCliente()
     {
         $clientes = Cliente::all();
-        return view('clientes.consultar', compact('clientes'));
+        return view('cliente.consultar', compact('clientes'));
     }
 
     public function editarCliente($id_cliente) {
         $cliente = Cliente::findOrFail($id_cliente);
-        return view('clientes.editar', compact('cliente'));
+        return view('cliente.editar', compact('cliente'));
     }
 
     public function atualizarCliente(Request $request) 
     {
         $cliente = $request->all();
         Cliente::findOrFail($request->id_cliente)->update($cliente);
-        return redirect('clientes/consultar');//->with('edicaoRealizada', 'Cadastro atualizado com sucesso!');
+        return redirect(route('cliente.consultar'));//->with('edicaoRealizada', 'Cadastro atualizado com sucesso!');
     }
 
     public function deletarCliente($id_cliente)
     {
         Cliente::findOrFail($id_cliente)->delete();
-        return redirect('clientes/consultar');//->with('exclusaoRealizada', 'Cadastro excluído com sucesso!');
+        return redirect(route('cliente.consultar'));//->with('exclusaoRealizada', 'Cadastro excluído com sucesso!');
     }
 }
