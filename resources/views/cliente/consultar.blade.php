b441276bd45312c876da83bde2aaa45844fa225f
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-11-19 16:28:04 -0300

    feat - Adicionado as views editar e consultar da área clientes

---
+++ resources/views/cliente/consultar.blade.php
@@ -0,0 +1,78 @@
+@extends('dashboard_main')
+
+@section('content')
+    <div class="info-section bg-body-tertiary">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+            <h2>Clientes</h2>    
+            <a href="{{ route('cliente.cadastro') }}" class="btn btn-success" id="btnCadastrar"><i class="bi bi-person-fill-add"></i>Novo Cliente</a>
+        </div>
+    </div>
+    <div class="painel-acoes">
+        <form action="{{ route('cliente.consultar') }}" method="GET" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-3 row">
+                <div class="col-md-6">
+                    <label for="nome" class="form-label col-md-2">Nome</label>
+                    <input type="text" class="form-control" id="nome" name="nome">
+                </div>
+                <div class="col-md-6">
+                    <label for="cpf" class="form-label col-md-2">CPF</label>
+                    <input type="text" class="form-control" id="cpf" name="cpf">
+                </div>
+            </div>
+            <div class="d-flex justify-content-end">
+                <button type="submit" class="btn btn-primary">Consultar</button>
+            </div>
+        </form>
+    </div>
+
+    @if( isset($clientes) )
+
+    <div class="info-section bg-body-tertiary">
+        <h2>Consulta de Clientes</h2>
+    </div>
+
+    @if(count($clientes) == 0)
+        <div class="alert alert-warning" role="alert">
+            <i class="bi bi-exclamation-triangle-fill"></i> Não há nenhum registro de cliente na base de dados!
+        </div>
+    @endif
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome</th>
+                <th scope="col">CPF</th>
+                <th scope="col">Telefone</th>
+                <th scope="col">Data Cadastro</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($clientes as $cliente)
+            <tr>
+                <th scope="row">{{$cliente->id_cliente}}</th>
+                <td>{{$cliente->nome}}</td>
+                <td>{{$cliente->cpf}}</td>
+                <td>{{$cliente->telefone}}</td>
+                <td>{{date('d/m/Y', strtotime($cliente->data_cadastro))}}</td>
+                <td class="d-flex gap-2">
+                    <a href="{{ route('cliente.editar', $cliente->id_cliente) }}" class="btn btn-secondary acoes" title="Editar">
+                       <i class="bi bi-pen"></i>
+                    </a>
+                    <form action="{{ route('cliente.deletar', $cliente->id_cliente) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger acoes" title="Deletar"><i class="bi bi-trash3"></i></button>
+                    </form>
+                </td>
+
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    @endif
+
+@endsection
--- resources/views/cliente/editar.blade.php
+++ resources/views/cliente/editar.blade.php
@@ -4,35 +4,36 @@
 
     <div class="info-section bg-body-tertiary">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
-            <h2>Clientes - Novo Cadastro</h2>    
-            <a href="{{ route('cliente.cadastro') }}" class="btn btn-success" id="btnCadastrar"><i class="bi bi-person-fill-add"></i>Novo Cliente</a>
+            <h2>Clientes - Editar Cadastro</h2>    
         </div>
     </div>
+
     <div class="painel-acoes">
-        <form action="{{ route('cliente.cadastrar') }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('cliente.atualizar', ['id_cliente' => $cliente->id_cliente]) }}" method="POST" enctype="multipart/form-data">
             @csrf
+            @method('PUT')
             <div class="mb-3 row">
                 <div class="col-md-6">
                     <label for="nome" class="form-label col-md-2">Nome</label>
-                    <input type="text" class="form-control" id="nome" name="nome">
+                    <input type="text" class="form-control" id="nome" name="nome" value="{{ $cliente->nome }}">
                 </div>
                 <div class="col-md-6">
                     <label for="cpf" class="form-label col-md-2">CPF</label>
-                    <input type="text" class="form-control" id="cpf" name="cpf">
+                    <input type="text" class="form-control" id="cpf" name="cpf" value="{{ $cliente->cpf }}">
                 </div>
             </div>
             <div class="mb-3 row">
                 <div class="col-md-6">
                     <label for="endereco" class="form-label col-md-2">Endereço</label>
-                    <input type="text" class="form-control" id="endereco" name="endereco">
+                    <input type="text" class="form-control" id="endereco" name="endereco" value="{{ $cliente->endereco }}">
                 </div>
                 <div class="col-md-6">
                     <label for="telefone" class="form-label col-md-2">Telefone</label>
-                    <input type="text" class="form-control" id="telefone" name="telefone">
+                    <input type="text" class="form-control" id="telefone" name="telefone" value="{{ $cliente->telefone }}">
                 </div>
             </div>
             <div class="d-flex justify-content-end">
-                <button type="submit" class="btn btn-primary">Cadastrar</button>
+                <button type="submit" class="btn btn-primary">Atualizar</button>
             </div>
         </form>
     </div>
