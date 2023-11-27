e4587c4dfd32e9ffee4988d8eb804fe9b097abf1
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-11-26 21:09:26 -0300

    fix - Ajuste na tela de cliente, alteracoes no ClienteController

--- resources/views/cliente/cadastro.blade.php
+++ resources/views/cliente/cadastro.blade.php
@@ -5,8 +5,7 @@
     <div class="info-section bg-body-tertiary d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
         <h2 class="mb-0"><i class="bi bi-person-fill me-2"></i>Clientes - Novo Cadastro</h2>
         <div class="d-flex gap-2">
-            <a href="{{ route('cliente.consultar') }}" class="btn btn-danger" id="btnVoltar"><i class="bi bi-arrow-left-circle-fill"></i>Voltar</a>
-            <a href="{{ route('cliente.cadastro') }}" class="btn btn-success" id="btnNovoCliente"><i class="bi bi-person-fill-add"></i>Novo Cliente</a>
+            <a href="{{ route('cliente.view-menu') }}" class="btn btn-danger" id="btnVoltar"><i class="bi bi-arrow-left-circle-fill"></i>Voltar</a>
         </div>
     </div>
     <div class="painel-acoes bg-body-tertiary p-3 info-section">
