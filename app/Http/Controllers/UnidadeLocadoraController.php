1af3bb65b956421c36b535cb0b2559b83ef6f2cb
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-11-29 01:07:51 -0300

    fix - Ajustes no UnidadeLocadora Controller

--- app/Http/Controllers/UnidadeLocadoraController.php
+++ app/Http/Controllers/UnidadeLocadoraController.php
@@ -6,43 +6,52 @@ use Illuminate\Http\Request;
 use App\Models\UnidadeLocadora;
 class UnidadeLocadoraController extends Controller
 {
+
+    public function create(){
+        return view('unidade.consultar');
+    }
+
+    public function createCadastro(){
+        return view('unidade.cadastro');
+    }
+
     public function cadastrarUnidadeLocadora(Request $request)
     {
         $unidadeLocadora = new UnidadeLocadora();
 
-        $unidadeLocadora->id_unidade_locadora = $request->id_unidade_locadora;
+        //$unidadeLocadora->id_unidade_locadora = $request->id_unidade_locadora;
         $unidadeLocadora->cidade = $request->cidade;
-        $unidadeLocadora->estado = $request->estado;
+        $unidadeLocadora->estado = $request->uf;
         $unidadeLocadora->id_funcionario = $request->id_funcionario;
-        $unidadeLocadora->data_cadastro = date('d/m/Y H:i');
+        $unidadeLocadora->data_cadastro = date('Y-m-d H:i:s');
 
         
         $unidadeLocadora->save();
 
-        return redirect('unidadeLocadora/cadastrar')->with('cadastroRealizado', 'Cadastro realizado com sucesso!');
+        return redirect(route('unidadeLocadora.view-menu'))->with('alertaSucesso', 'Cadastro realizado com sucesso!');
     }
 
     public function consultarUnidadeLocadora()
     {
         $unidadesLocadora = UnidadeLocadora::all();
-        return view('unidadeLocadora.consultar', compact('unidadesLocadoras'));
+        return view('unidade.consultar', compact('unidadesLocadora'));
     }
 
     public function editarUnidadeLocadora($id_unidade_locadora) {
         $unidadeLocadora = UnidadeLocadora::findOrFail($id_unidade_locadora);
-        return view('unidadeLocadora.editar', compact('unidadeLocadora'));
+        return view('unidade.editar', compact('unidadeLocadora'));
     }
 
     public function atualizarUnidadeLocadora(Request $request) 
     {
         $unidadeLocadora = $request->all();
         UnidadeLocadora::findOrFail($request->id_unidade_locadora)->update($unidadeLocadora);
-        return redirect('unidadeLocadora/consultar');//->with('edicaoRealizada', 'Cadastro atualizado com sucesso!');
+        return redirect(route('unidadeLocadora.consultar'))->with('alertaSucesso', 'Cadastro atualizado com sucesso!');
     }
 
     public function deletarUnidadeLocadora($id_unidade_locadora)
     {
         UnidadeLocadora::findOrFail($id_unidade_locadora)->delete();
-        return redirect('unidadeLocadora/consultar');//->with('exclusaoRealizada', 'Cadastro excluído com sucesso!');
+        return redirect(route('unidadeLocadora.consultar'))->with('alertaSucesso', 'Cadastro excluído com sucesso!');
     }
 }
