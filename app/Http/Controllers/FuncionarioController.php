9c59c00cd27a0662858a16b0bd009544d68f7a5a
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-11-29 00:16:59 -0300

    fix - Ajustes no FuncinarioController

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -7,20 +7,28 @@ use App\Models\Funcionario;
 
 class FuncionarioController extends Controller
 {
+
+    public function create(){
+        return view('funcionario.consultar');
+    }
+
+    public function createCadastro(){
+        return view('funcionario.cadastro');
+    }
+
     public function cadastrarFuncionario(Request $request)
     {
         $funcionario = new Funcionario();
 
-        $funcionario->id_funcionario = $request->id_funcionario;
+        //$funcionario->id_funcionario = $request->id_funcionario;
         $funcionario->id_unidade_locadora = $request->id_unidade_locadora;
         $funcionario->id_cargo = $request->id_cargo;
         $funcionario->nome = $request->nome;
-        $funcionario->data_cadastro = date('d/m/Y H:i');
+        $funcionario->data_cadastro = date('Y-m-d H:i:s');
 
-        
         $funcionario->save();
 
-        return redirect('funcionario/cadastrar')->with('cadastroRealizado', 'Cadastro realizado com sucesso!');
+        return redirect(route('funcionario.view-menu'))->with('alertaSucesso', 'Cadastro realizado com sucesso!');
     }
 
     public function consultarFuncionario()
@@ -38,12 +46,12 @@ class FuncionarioController extends Controller
     {
         $funcionario = $request->all();
         Funcionario::findOrFail($request->id_funcionario)->update($funcionario);
-        return redirect('funcionario/consultar');
+        return redirect(route('funcionario.consultar'))->with('alertaSucesso', 'Cadastro realizado com sucesso!');
     }
 
     public function deletarFuncionario($id_funcionario)
     {
         Funcionario::findOrFail($id_funcionario)->delete();
-        return redirect('funcionario/consultar');
+        return redirect(route('funcionario.consultar'))->with('alertaSucesso', 'Cadastro realizado com sucesso!');
     }
 }
