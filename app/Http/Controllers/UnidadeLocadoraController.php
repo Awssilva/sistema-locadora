1b0d1b640b5df17a64d5b52d423205090345fe5f
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-11-30 11:58:32 -0300

    feat - Ajuste nos metodos do CRUD para carregar as informacoes de usuarios nos selects

--- app/Http/Controllers/UnidadeLocadoraController.php
+++ app/Http/Controllers/UnidadeLocadoraController.php
@@ -4,44 +4,55 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UnidadeLocadora;
+use App\Models\Funcionario;
 class UnidadeLocadoraController extends Controller
 {
 
-    public function create(){
-        return view('unidade.consultar');
+    //Método responsável por carregar a View de Consulta do módulo Unidades.
+    public function create()
+    {
+        $funcionarios = Funcionario::getAllFuncionarios();
+        return view('unidade.consultar', compact('funcionarios'));
     }
 
-    public function createCadastro(){
-        return view('unidade.cadastro');
+    //Método responsável por carregar a View de Cadastro do módulo Unidades.
+    public function createCadastro()
+    {
+       $funcionarios = Funcionario::getAllFuncionarios();
+        return view('unidade.cadastro', compact('funcionarios'));
     }
 
+    //Método responsável por realizar ação de cadastro de dados do módulo Unidades.
     public function cadastrarUnidadeLocadora(Request $request)
     {
         $unidadeLocadora = new UnidadeLocadora();
 
-        //$unidadeLocadora->id_unidade_locadora = $request->id_unidade_locadora;
         $unidadeLocadora->cidade = $request->cidade;
         $unidadeLocadora->estado = $request->estado;
         $unidadeLocadora->id_funcionario = $request->id_funcionario;
         $unidadeLocadora->data_cadastro = date('Y-m-d H:i:s');
 
-        
         $unidadeLocadora->save();
 
         return redirect(route('unidadeLocadora.view-menu'))->with('alertaSucesso', 'Cadastro realizado com sucesso!');
     }
 
+    //Método responsável por realizar ação de consulta de dados do módulo Unidades.
     public function consultarUnidadeLocadora()
     {
-        $unidadesLocadora = UnidadeLocadora::all();
-        return view('unidade.consultar', compact('unidadesLocadora'));
+        $unidadesLocadora = UnidadeLocadora::getAllUnidades();
+        $funcionarios = Funcionario::getAllFuncionarios();
+        return view('unidade.consultar', compact('unidadesLocadora', 'funcionarios'));
     }
 
+    //Método responsável por carregar a View de Edição do módulo Unidades.
     public function editarUnidadeLocadora($id_unidade_locadora) {
         $unidadeLocadora = UnidadeLocadora::findOrFail($id_unidade_locadora);
-        return view('unidade.editar', compact('unidadeLocadora'));
+        $funcionarios = Funcionario::getFuncionarioJoinUnidadeLocadora();
+        return view('unidade.editar', compact('unidadeLocadora', 'funcionarios'));
     }
 
+    //Método responsável por realizar ação de atualização de dados do módulo Unidades.
     public function atualizarUnidadeLocadora(Request $request) 
     {
         $unidadeLocadora = $request->all();
@@ -49,6 +60,7 @@ class UnidadeLocadoraController extends Controller
         return redirect(route('unidadeLocadora.consultar'))->with('alertaSucesso', 'Cadastro atualizado com sucesso!');
     }
 
+    //Método responsável por realizar ação de exclusão de dados do módulo Unidades.
     public function deletarUnidadeLocadora($id_unidade_locadora)
     {
         UnidadeLocadora::findOrFail($id_unidade_locadora)->delete();
