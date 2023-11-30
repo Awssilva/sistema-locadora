dc12b3fe30f3692c7f8ab6d689a7fcb0c23d9f28
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-11-30 19:17:50 -0300

    feat - Implementado carregamento de dados nos selects das views do modulo Funcionario

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -4,18 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Funcionario;
+use App\Models\UnidadeLocadora;
 
 class FuncionarioController extends Controller
 {
-
-    public function create(){
-        return view('funcionario.consultar');
+    //Método responsável por carregar a View de Consulta do módulo Funcionário.
+    public function create()
+    {
+        $unidadesLocadora = UnidadeLocadora::getAllUnidades();
+        return view('funcionario.consultar', compact('unidadesLocadora'));
     }
 
-    public function createCadastro(){
-        return view('funcionario.cadastro');
+    //Método responsável por carregar a View de Cadastro do módulo Funcinário.
+    public function createCadastro()
+    {
+        $unidadesLocadora = UnidadeLocadora::getAllUnidades();
+        return view('funcionario.cadastro', compact('unidadesLocadora'));
     }
 
+    //Método responsável por realizar ação de cadastro de dados do módulo Funcionário.
     public function cadastrarFuncionario(Request $request)
     {
         $funcionario = new Funcionario();
@@ -31,17 +38,22 @@ class FuncionarioController extends Controller
         return redirect(route('funcionario.view-menu'))->with('alertaSucesso', 'Cadastro realizado com sucesso!');
     }
 
+    //Método responsável por realizar ação de consulta de dados do módulo Funcionário.
     public function consultarFuncionario()
     {
-        $funcionarios = Funcionario::all();
-        return view('funcionario.consultar', compact('funcionarios'));
+        $unidadesLocadora = UnidadeLocadora::getAllUnidades();
+        $funcionarios = Funcionario::getFuncionarioJoinUnidadeLocadora();
+        return view('funcionario.consultar', compact('funcionarios',  'unidadesLocadora'));
     }
-
+    
+    //Método responsável por carregar a View de Edição do módulo Funcionário.
     public function editarFuncionario($id_funcionario) {
         $funcionario = Funcionario::findOrFail($id_funcionario);
-        return view('funcionario.editar', compact('funcionario'));
+        $unidadesLocadora = UnidadeLocadora::getAllUnidades();
+        return view('funcionario.editar', compact('funcionario', 'unidadesLocadora'));
     }
 
+    //Método responsável por realizar ação de atualização de dados do módulo Funcionário.
     public function atualizarFuncionario(Request $request) 
     {
         $funcionario = $request->all();
@@ -49,6 +61,7 @@ class FuncionarioController extends Controller
         return redirect(route('funcionario.consultar'))->with('alertaSucesso', 'Cadastro realizado com sucesso!');
     }
 
+    //Método responsável por realizar ação de exclusão de dados do módulo Funcionário.
     public function deletarFuncionario($id_funcionario)
     {
         Funcionario::findOrFail($id_funcionario)->delete();
