5ae7645ff1d3d9a1f1e92a184a3bf83762b67b3e
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-11-30 00:09:16 -0300

    fix - Ajuste na tela de cadastro de veiculo

--- resources/views/veiculo/cadastro.blade.php
+++ resources/views/veiculo/cadastro.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="info-section bg-body-tertiary d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
-        <h2 class="mb-0"><i class="bi bi-car-front-fill me-2"></i>Veículos</h2>
+        <h2 class="mb-0"><i class="bi bi-car-front-fill me-2"></i>Veículos - Novo cadastro</h2>
         <div class="d-flex gap-2">
             <a href="{{ route('veiculo.view-menu') }}" class="btn btn-danger" id="btnVoltar"><i class="bi bi-arrow-left-circle-fill"></i>Voltar</a>
         </div>
