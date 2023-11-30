55df204150682d4670fa2687363b6593e7846618
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-11-30 11:23:28 -0300

    feat - Adicionado os metotodos: getAllFuncionarios, getFuncionarioJoinUnidadeLocadora, getFuncionarioByIdFuncionario ao model Funcionario.

--- app/Models/Funcionario.php
+++ app/Models/Funcionario.php
@@ -13,4 +13,26 @@ class Funcionario extends Model
     protected $primaryKey = 'id_funcionario';
     protected $guarded = [];  
     public $timestamps = false;
+    
+    //Método para retornar todos os registros e informações da tabela funcionario.
+    public static function getAllFuncionarios()
+    {
+        $result = self::all();
+        return $result;
+    }
+
+    //Método para retornar todos os registros e informações de funcionarios que que estão registrados em Unidade.
+    public static function getFuncionarioJoinUnidadeLocadora()
+    {
+        $result = self::join('unidade_locadora', 'funcionario.id_funcionario', '=', 'unidade_locadora.id_funcionario')
+                ->get();
+        return $result;
+    }
+    
+    //Método para retornar as informações de um funcionário a partir de seu id_funcioanrio.
+    public static function getFuncionarioByIdFuncionario($id_funcionario)
+    {
+        $result = Funcionario::where('id_funcionario', $id_funcionario)->get();
+        return $result;
+    }    
 }
