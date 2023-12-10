9a9bd1d529cf94b170a8411c417b2a65a679e890
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2023-12-10 13:27:52 -0300

    feat - novas alteracoes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,19 +9,24 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    // Método para exibir o dashboard
     public function create(){
 
+        // Obtém a data atual no formato 'd/m/Y'
         $data_atual = date('d/m/Y');
+
+        // Obtém todos os registros de veículos, clientes e locações
         $veiculos = Veiculo::all();
         $clientes = Cliente::all();
         $locacoes = Locacao::all();
 
+        // Calcula o número de veículos, clientes, locações e veículos disponíveis
         $count_veiculos = !empty($veiculos) ? count($veiculos) : 'Sem registros' ;
         $count_clientes = !empty($clientes) ? count($clientes) : 'Sem registros' ;
         $count_locacoes = !empty($locacoes) ? count($locacoes) : 'Sem registros' ;
         $count_veiculos_disponiveis = $count_veiculos - $count_locacoes;
 
-
+        // Cria um array compacto com as variáveis para serem passadas para a view
         $compact = compact('data_atual', 
                             'count_veiculos',
                             'count_clientes',
@@ -29,6 +34,8 @@ class DashboardController extends Controller
                             'count_veiculos_disponiveis'
         );
 
+        // Retorna a view 'dashboard' com os dados compactos
         return view('dashboard', $compact);
     }
 }
+
--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -9,13 +9,22 @@ class Cliente extends Model
 {
     use HasFactory;
 
+    // Define o nome da tabela no banco de dados associada a este modelo
     protected $table = 'cliente';
+
+    // Define a chave primária da tabela
     protected $primaryKey = 'id_cliente';
-    protected $guarded = [];  
+
+    // Define quais atributos não podem ser atribuídos em massa (mass-assignment)
+    protected $guarded = [];
+
+    // Desativa a criação automática de timestamps (created_at e updated_at)
     public $timestamps = false;
 
+    // Método estático para obter clientes com base no nome e/ou CPF
     public static function getClientesByNomeCpf($nome, $cpf)
     {
+        // Cria uma instância da query usando o próprio modelo
         $query = self::select('cliente.*');
 
         // Adiciona condição para nome do cliente se não for vazio
@@ -28,9 +37,11 @@ class Cliente extends Model
             $query->orWhere('cliente.cpf', 'like', '%' . $cpf . '%');
         }
 
+        // Executa a query e obtém os resultados
         $result = $query->get();
 
+        // Retorna os resultados
         return $result;
     }
-
 }
+
